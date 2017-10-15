d5a5e97662e0a51e690ec6ce2811110679917473
Avelino <t@avelino.xxx>
2017-10-15 15:39:10 -0200

    Publishing to master (publish.sh)

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -1,24 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo "Removing existing files"
+rm -rf public
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+echo "Generating site"
+hugo
 
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
-# Come Back up to the Project Root
-cd ..
+echo "Updating master branch"
+cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
