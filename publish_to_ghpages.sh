7e7a3e7276d648901956ba383061b946398de777
Avelino <t@avelino.xxx>
2017-10-15 15:55:47 -0200

    removed all files on deploy

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -7,4 +7,4 @@ echo "Generating site"
 hugo
 
 echo "Updating master branch"
-git checkout master && mv public/* . && git add . && git commit -m "Publishing to master (publish.sh)"
+git checkout master && ll --ignore public | xargs rm -rf && mv public/* . && rm -rf public && git add . && git commit -m "Publishing to master (publish.sh)"
