2151f232c1cfa7d4ebd63b135a8f7e75a011b6db
Avelino <t@avelino.xxx>
2017-10-15 16:01:05 -0200

    fixed deploy script by @avelino

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+echo "Removing existing files"
+rm -rf public
 
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
+echo "Generating site"
+hugo
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
+echo "Updating master branch"
+msg="rebuilding site `date`, publishing to master"
+git checkout master && ll --ignore public | xargs rm -rf && cp -rf public/* . && rm -rf public && git add . && git commit -m "$msg"
 
-# Push source and build repos.
 git push origin master
 
-# Come Back up to the Project Root
-cd ..
+git checkout source
