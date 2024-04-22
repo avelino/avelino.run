ee426d734c217ec939524def10b0141e713a817c
Avelino <31996+avelino@users.noreply.github.com>
2024-04-22 10:57:53 -0300

    activitypub-utils multi os
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- netlify-release.sh
+++ netlify-release.sh
@@ -3,13 +3,21 @@ git submodule update -f --init
 hugo
 
 # static-activitypub
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     release_os="linux-x64";;
+    Darwin*)    release_os="osx-x64";;
+    *)          release_os="UNKNOWN:${unameOut}"
+esac
 release_version="0.0.5"
-release_name="activitypub-utils-$release_version-linux-x64"
+release_name="activitypub-utils-$release_version-$release_os"
 wget "https://github.com/mahomedalid/almost-static-activitypub/releases/download/$release_version/$release_name.tar.gz"
 tar -xf "$release_name.tar.gz"
 
-$release_name/Rss2Outbox \
-  --rssPath ./public/index.xml \
-  --staticPath ./public \
+cd $release_name
+./Rss2Outbox \
+  --rssPath ../public/index.xml \
+  --staticPath ../public \
   --siteActorUri "https://avelino.run/\@hey" \
   --authorUsername "\@hey\@avelino.run"
+cd ..
