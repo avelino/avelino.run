7c7c061a9555eb4b7cfde3e933a5b8e2e8ab7193
Avelino <31996+avelino@users.noreply.github.com>
2024-04-22 10:05:32 -0300

    static-activitypub in deploy, read feed/rss
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

---
+++ netlify-release.sh
@@ -0,0 +1,14 @@
+git submodule update -f --init
+hugo
+
+# static-activitypub
+release_version="0.0.5"
+release_name="activitypub-utils-$release_version-linux-x64"
+wget "https://github.com/mahomedalid/almost-static-activitypub/releases/download/$release_version/$release_name.tar.gz"
+tar -xf "$release_name.tar.gz"
+
+$release_name/Rss2Outbox \
+  --rssPath ./public/index.xml \
+  --staticPath ./public \
+  --siteActorUri "https://avelino.run/\@hey" \
+  --authorUsername "\@hey\@avelino.run"
