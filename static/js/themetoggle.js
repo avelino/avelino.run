1cf75b43c4ffbc7e9835ec83077e31e3ff007015
Avelino <31996+avelino@users.noreply.github.com>
2026-01-29 10:59:42 -0300

    fixed dark mod
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

---
+++ static/js/themetoggle.js
@@ -0,0 +1,24 @@
+function setTheme(mode) {
+    localStorage.setItem("theme-storage", mode);
+    var e = document.querySelector("#dark-mode-toggle > .feather > use")
+    if (mode === "dark") {
+        document.getElementById("darkModeStyle").disabled=false;
+        document.body.classList.add("dark-theme");
+        e.href.baseVal = e.href.baseVal.replace(/#.*$/, "#sun")
+    } else if (mode === "light") {
+        document.getElementById("darkModeStyle").disabled=true;
+        document.body.classList.remove("dark-theme");
+        e.href.baseVal = e.href.baseVal.replace(/#.*$/, "#moon")
+    }
+}
+
+function toggleTheme() {
+    if (localStorage.getItem("theme-storage") === "light") {
+        setTheme("dark");
+    } else if (localStorage.getItem("theme-storage") === "dark") {
+        setTheme("light");
+    }
+}
+
+var savedTheme = localStorage.getItem("theme-storage") || "light";
+setTheme(savedTheme);
